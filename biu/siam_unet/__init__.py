b659df60890ed90b44ab89b72d78072dd1c2e5d0
Yuxi Long <longyuxi@live.com>
2021-08-12 10:05:19 +0800

    removed pytorch dependency

--- biu/siam_unet/__init__.py
+++ biu/siam_unet/__init__.py
@@ -1,4 +1,8 @@
 import os
+try:
+    import torch
+except ImportError:
+    raise ImportError # Torch is not installed. Please install through https://pytorch.org/get-started/locally/
 from .data import DataProcess
 from .train import Trainer
 from .predict import Predict
