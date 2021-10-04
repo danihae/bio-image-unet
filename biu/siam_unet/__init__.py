3874fda573d5b4c3764c81de4dc30f84c4e62099
Yuxi Long <longyuxi@live.com>
2021-10-04 14:09:52 -0400

    added check for torch version

--- biu/siam_unet/__init__.py
+++ biu/siam_unet/__init__.py
@@ -2,6 +2,12 @@ try:
     import torch
 except ImportError:
     raise ImportError('Torch is not installed. Please install through https://pytorch.org/get-started/locally/')
+
+# check if torch version >= 1.9.0
+from packaging import version
+if not version.parse(torch.__version__) >= version.parse('1.9.0'):
+    raise ImportError(f'This package requires torch >=1.9.0 to run. You have version {torch.__version__}.Please install through https://pytorch.org/get-started/locally/')
+
 from .data import DataProcess
 from .losses import *
 from .predict import Predict
